df000ab3255dc021de2459d13f9b69c0d2c306fc
Sergio <sergiokv13@gmail.com>
2017-08-14 15:49:24 -0400

    asd

--- app/models/voto.rb
+++ app/models/voto.rb
@@ -6,9 +6,9 @@ class Voto < ApplicationRecord
 		self.direccion_votante =  res
 	end
 
-	def self.emitir_voto(direccion_partido)
-		s = direccion_partido
-		cmd = "multichain-cli cadena sendwithdatafrom " + Voto.last.direccion_votante.to_s.strip  + " " + direccion_partido.to_s.strip + " '{" + '"balotas"' + ":1}' " + "'{" + '"for":"root","key":' + '"mesa'+ Estado.last.id_en_linea.to_s + '","data":"' + s.unpack('U'*s.length).collect {|x| x.to_s 16}.join.to_s + '"' + "}'"
+	def self.emitir_voto(direccion_opcion)
+		s = Estado.last.id_en_linea
+		cmd = "multichain-cli cadena sendwithdatafrom " + Voto.last.direccion_votante.to_s.strip  + " " + direccion_opcion.to_s.strip + " '{" + '"balotas"' + ":1}' " + "'{" + '"for":"root","key":' + '"'+ direccion_opcion + '","data":"' + s.unpack('U'*s.length).collect {|x| x.to_s 16}.join.to_s + '"' + "}'"
 		puts "DIRECCIOOOOOON-" + cmd
 		res = %x[#{cmd}]
 	end
