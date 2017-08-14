6ca9a408a830fc15d89fdc4d8fa29c2eb5ab8e5f
Sergio <sergiokv13@gmail.com>
2017-08-14 14:28:34 -0400

    asd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    
 end
