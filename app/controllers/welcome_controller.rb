ff9f669e63b8bc7466f6d880abf82b99dc122a70
Sergio <sergiokv13@gmail.com>
2017-08-14 13:24:19 -0400

    ASd

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -20,7 +20,8 @@ class WelcomeController < ApplicationController
     voto = Voto.new
     voto.set_votante
     voto.save
-    url = URI.parse('http://104.131.40.8/informar_direccion/' + voto.direccion_votante)
+    encoded_url = URI.encode('http://104.131.40.8/informar_direccion/' + voto.direccion_votante)
+    url = URI.parse(encoded_url)
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
